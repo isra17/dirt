ec5aa60cd6c7e3edbdd8da10674b75457d0f77d8
isra17 <isra017@gmail.com>
2016-07-03 22:25:31 -0400

    Switch tests to atoi

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -7,8 +7,8 @@ use emu::vmstate::VmState;
 use self::target_rules::{RuleVerifier, TargetRules};
 use std::rc::Rc;
 
-struct StrcmpRule;
-impl RuleVerifier for StrcmpRule {
+struct AtoiRule;
+impl RuleVerifier for AtoiRule {
     fn verify(&self, effects: &EmuEffects, vmstate: &VmState) -> bool {
         let pushed_args = effects.args.pushed_args();
         let a = match vmstate.read_str(pushed_args[0]) {
@@ -16,32 +16,23 @@ impl RuleVerifier for StrcmpRule {
             Err(_) => return false,
         };
 
-        let b = match vmstate.read_str(pushed_args[1]) {
-            Ok(x) => x,
-            Err(_) => return false,
-        };
+        let n: i32 = a.parse().unwrap();
 
-        if a == b {
-            return effects.return_value == 0;
-        } else {
-            return effects.return_value != 0;
-        }
+        return effects.return_value as i32 == n;
     }
 }
 
 pub fn fixtures() -> ruleset::RuleSet {
     use emu::datatypes::StringData;
-    let inputs = vec![EmuArgs::new(vec![Rc::new(StringData::new("a")),
-                                        Rc::new(StringData::new("a"))]),
-                      EmuArgs::new(vec![Rc::new(StringData::new("a")),
-                                        Rc::new(StringData::new("b"))])];
+    let inputs = vec![EmuArgs::new(vec![Rc::new(StringData::new("123"))]),
+                      EmuArgs::new(vec![Rc::new(StringData::new("-12"))])];
 
-    let strcmp_rules = TargetRules {
-        name: String::from("strcmp"),
+    let atoi_rules = TargetRules {
+        name: String::from("atoi"),
         inputs: inputs,
-        verifier: Box::new(StrcmpRule {}),
+        verifier: Box::new(AtoiRule {}),
     };
 
-    let ruleset = ruleset::RuleSet { candidates_rules: vec![strcmp_rules] };
+    let ruleset = ruleset::RuleSet { candidates_rules: vec![atoi_rules] };
     return ruleset;
 }

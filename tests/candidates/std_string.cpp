a35fafb60154707535161f7e99d4fd3c158205d2
isra17 <isra017@gmail.com>
2016-07-13 20:11:00 -0400

    Update test build

--- tests/candidates/std_string.cpp
+++ tests/candidates/std_string.cpp
@@ -27,7 +27,7 @@ CPP_CANDIDATE(std::string, std_string_string_fill, string(n,c))
 (std::string *self, size_t n, char c) { return new (self)std::string(n, c); }
 
 CPP_CANDIDATE(std::string, std_string_string_append_cstring, append(s))
-(std::string *self, char* s) { return new self->append(s); }
+(std::string *self, char* s) { return &self->append(s); }
 
 }
 

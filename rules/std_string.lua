62f1eedf95cd3801036bc63d4480a2861494e611
isra17 <isra017@gmail.com>
2016-07-14 20:41:03 -0400

    More strings tests

--- rules/std_string.lua
+++ rules/std_string.lua
@@ -1,3 +1,18 @@
+--[[
+-- GCC libstdc++ strings looks like:
+-- struct std::string {
+--   char* data;
+--   size_t length;
+--   enum {
+--     char local_buf[16];
+--     size_t capacity;
+--   };
+-- };
+-- where `data` always point to a string, `length` always contains the exact
+-- string length and the last enum is either the string buffer if `length` < 16
+-- or the allocated buffer capacity.
+--]]
+
 StdString = {}
 StdString.mt = {
   __index = {
@@ -8,17 +23,40 @@ StdString.mt = {
   }
 }
 
-StdString.struct = {Dirt.Ptr, Dirt.U, Dirt.Buf(0x10)}
+StdString.sizeof = 0x10
 
-StdString.new = function(str) return "" end
+function StdString.new(str)
+  if str == "" then return Dirt.Buf(0x10) end
+  if str:len() < 16 then
+    local buf = Dirt.Str(str)
+    return {buf:ptr(), Dirt.U(0), buf}
+  else
+    local buf = Dirt.Str(str)
+    return {buf:ptr(), Dirt.U(0), Dirt.U(str:len()), buf}
+  end
+end
 
-StdString.from = function(s, addr)
+function StdString.from(s, addr)
   local t = {s=s, a=addr}
   setmetatable(t, StdString.mt)
   return t;
 end
 
 Dirt.rule("std::string::string()",
-          Dirt.Buf(0x10),
+          Dirt.Buf(StdString.sizeof),
           function(s) return StdString.from(s, s:arg(0)):isEmpty() end)
 
+Dirt.rule("std::string::string(char*)",
+          "aa",
+          function(s) return StdString.from(s, s:arg(0)):str() == "aa" end)
+
+--[[
+Dirt.rule("std::string::string(std::string*)",
+          StdString.new("aa"),
+          function(s) return StdString.from(s, s:arg(0)):str() == "aa" end)
+
+Dirt.rule("std::string::__add(std::string*)",
+          StdString.new("aa"),
+          StdString.new("bb"),
+          function(s) return StdString.from(s, s:arg(0)):str() == "aabb" end)
+--]]
--- tests/candidates/std_string.cpp
+++ tests/candidates/std_string.cpp
@@ -3,8 +3,13 @@
 extern "C" {
 
 extern const char test_std_string_string_expect[] = "std::string::string()";
-extern std::string* test_std_string_string_fn(std::string *s) {
-  return new (s)std::string();
+extern std::string* test_std_string_string_fn(std::string *self) {
+  return new (self)std::string();
+}
+
+extern const char test_std_string_string_char_expect[] = "std::string::string(char*)";
+extern std::string* test_std_string_string_char_fn(std::string* self, char *s) {
+  return new (self)std::string(s);
 }
 
 }

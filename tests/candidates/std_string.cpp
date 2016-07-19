05026350282619169cef1b27d441af0b8b9ab8d7
isra17 <isra017@gmail.com>
2016-07-18 21:53:37 -0400

    Add more string test case

--- tests/candidates/std_string.cpp
+++ tests/candidates/std_string.cpp
@@ -12,6 +12,16 @@ extern std::string* test_std_string_string_char_fn(std::string* self, char *s) {
   return new (self)std::string(s);
 }
 
+extern const char test_std_string_string_string_expect[] = "std::string::string(std::string*)";
+extern std::string* test_std_string_string_string_fn(std::string* self, std::string *s) {
+  return new (self)std::string(*s);
+}
+
+extern const char test_std_string_char_char_expect[] = "std::string::append(char*)";
+extern std::string* test_std_string_char_char_fn(std::string* self, char *s) {
+  return &self->append(s);
+}
+
 }
 
 int main() {

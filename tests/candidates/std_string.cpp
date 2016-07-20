77b349d034e14e29845eae9e9fcbf1ef80330cbd
isra17 <isra017@gmail.com>
2016-07-19 20:31:07 -0400

    Cleanify cpp test file

--- tests/candidates/std_string.cpp
+++ tests/candidates/std_string.cpp
@@ -2,25 +2,21 @@
 
 extern "C" {
 
-extern const char test_std_string_string_expect[] = "std::string::string()";
-extern std::string* test_std_string_string_fn(std::string *self) {
-  return new (self)std::string();
-}
+#define CPP_CANDIDATE(CLASS, NAME, DESC, ...) \
+  extern const char test_##NAME##_expect[] = #CLASS "::" #DESC; \
+  extern CLASS* test_##NAME##_fn
 
-extern const char test_std_string_string_char_expect[] = "std::string::string(char*)";
-extern std::string* test_std_string_string_char_fn(std::string* self, char *s) {
-  return new (self)std::string(s);
-}
+CPP_CANDIDATE(std::string, std_string_string, string())
+(std::string *self) { return new (self)std::string(); }
 
-extern const char test_std_string_string_string_expect[] = "std::string::string(std::string*)";
-extern std::string* test_std_string_string_string_fn(std::string* self, std::string *s) {
-  return new (self)std::string(*s);
-}
+CPP_CANDIDATE(std::string, std_string_string_char, string(char*))
+(std::string *self, char* s) { return new (self)std::string(s); }
 
-extern const char test_std_string_char_char_expect[] = "std::string::append(char*)";
-extern std::string* test_std_string_char_char_fn(std::string* self, char *s) {
-  return &self->append(s);
-}
+CPP_CANDIDATE(std::string, std_string_string_string, string(std::string*))
+(std::string *self, std::string* s) { return new (self)std::string(*s); }
+
+CPP_CANDIDATE(std::string, std_string_string_append_char, string(char*))
+(std::string *self, char* s) { return new (self)std::string(s); }
 
 }
 

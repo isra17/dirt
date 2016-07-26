7a60aba7df3a8a266c3b9c24167bc7e99c4b40fb
isra17 <isra017@gmail.com>
2016-07-25 21:25:11 -0400

    ...

--- travis/install_unicorn.sh
+++ travis/install_unicorn.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -e
-export UNICORN_QEMU_FLAGS="--python=$(which python2)"
-wget https://github.com/unicorn-engine/unicorn/archive/0.9.tar.gz
-tar zxf 0.9.tar.gz
-cd unicorn-0.9
-sudo ./make.sh install
+git clone --branch 0.9 git@github.com:unicorn-engine/unicorn.git
+cd unicorn
+UNICORN_ARCHS="x86" ./make.sh clang

632daeef4d2cdd75b4a5446183526c628f87b989
isra17 <isra017@gmail.com>
2016-07-25 22:02:51 -0400

    more tests

--- travis/unicorn.sh
+++ travis/unicorn.sh
@@ -2,6 +2,6 @@
 set -e
 if [ ! -d "unicorn" ]; then
   git clone --branch 0.9 https://github.com/unicorn-engine/unicorn.git
-  cd unicorn
-  UNICORN_ARCHS="x86" ./make.sh clang
 fi
+cd unicorn
+UNICORN_ARCHS="x86" ./make.sh clang install

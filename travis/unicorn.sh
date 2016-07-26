62df9b707fa4a1a0a654ea86a1350fda01f88f93
isra17 <isra017@gmail.com>
2016-07-25 22:20:34 -0400

    Add clang

--- travis/unicorn.sh
+++ travis/unicorn.sh
@@ -5,5 +5,5 @@ if [ ! -d "unicorn/*.so" ]; then
   git clone --branch 0.9 https://github.com/unicorn-engine/unicorn.git
 fi
 cd unicorn
-sudo ./make.sh clang
+./make.sh clang
 sudo ./make.sh install

748513a1967e56a21eeda481fd3443ccfcc507e4
isra17 <isra017@gmail.com>
2016-07-25 21:53:42 -0400

    Ugh

--- travis/capstone.sh
+++ travis/capstone.sh
@@ -3,5 +3,3 @@ set -e
 git clone --branch 3.0.4 https://github.com/aquynh/capstone.git
 cd capstone
 CAPSTONE_ARCHS="x86" ./make.sh clang
-sudo ./make.sh install
-
--- travis/unicorn.sh
+++ travis/unicorn.sh
@@ -3,4 +3,3 @@ set -e
 git clone --branch 0.9 https://github.com/unicorn-engine/unicorn.git
 cd unicorn
 UNICORN_ARCHS="x86" ./make.sh clang
-sudo ./make.sh install

6d23d6438fdf236611287866056fae7efca39c65
isra17 <isra017@gmail.com>
2016-07-26 12:38:18 -0400

    Use master unicorn

--- travis/unicorn.sh
+++ travis/unicorn.sh
@@ -2,7 +2,7 @@
 set -e
 if [ ! -d "unicorn/*.so" ]; then
   rm -rf unicorn
-  git clone --branch 0.9 https://github.com/unicorn-engine/unicorn.git
+  git clone https://github.com/unicorn-engine/unicorn.git
 fi
 cd unicorn
 ./make.sh clang

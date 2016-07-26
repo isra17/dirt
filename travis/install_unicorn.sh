29e82e402bf2136ebae322f7c56d2ce2b59a2427
isra17 <isra017@gmail.com>
2016-07-25 21:37:42 -0400

    install unicorn

--- travis/install_unicorn.sh
+++ travis/install_unicorn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-export LD_LIBRARY_PATH=`pwd`:$LD_LIBRARY_PATH
 git clone --branch 0.9 https://github.com/unicorn-engine/unicorn.git
 cd unicorn
 UNICORN_ARCHS="x86" ./make.sh clang
+sudo ./make install

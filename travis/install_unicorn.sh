899b6b9b66c5ff7c079d5a6ef67bcb2cf267a56a
isra17 <isra017@gmail.com>
2016-07-25 20:20:37 -0400

    Try travis fix

--- travis/install_unicorn.sh
+++ travis/install_unicorn.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 wget https://github.com/unicorn-engine/unicorn/archive/0.9.tar.gz
 tar zxvf 0.9.tar.gz
+cd unicorn-0.9
 UNICORN_ARCHS="x86" ./make.sh
-sudo ./make.sh install
+./make.sh install

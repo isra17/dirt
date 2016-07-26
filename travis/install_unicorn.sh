615708788c98415a49d9cc2a04a8ffbbad7dfb09
isra17 <isra017@gmail.com>
2016-07-25 20:38:45 -0400

    Unicorn fail

--- travis/install_unicorn.sh
+++ travis/install_unicorn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 wget https://github.com/unicorn-engine/unicorn/archive/0.9.tar.gz
-tar zxvf 0.9.tar.gz
+tar zxf 0.9.tar.gz
 cd unicorn-0.9
-UNICORN_ARCHS="x86" ./make.sh
+./make.sh
 ./make.sh install

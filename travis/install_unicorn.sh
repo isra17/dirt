22f12db0726df4a22c331058b6bf3da9f807198c
isra17 <isra017@gmail.com>
2016-07-25 21:00:03 -0400

    Set unicorn compiler

--- travis/install_unicorn.sh
+++ travis/install_unicorn.sh
@@ -3,7 +3,7 @@ set -e
 wget https://github.com/unicorn-engine/unicorn/archive/0.9.tar.gz
 tar zxf 0.9.tar.gz
 cd unicorn-0.9
-echo $CC
-echo `which cc`
+
+export CC=gcc
 ./make.sh
 ./make.sh install
